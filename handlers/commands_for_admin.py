c2793cc79bb57b2a42e50d11924288220dcb4e35
Gruvrr <bliznyuk.anton@gmail.com>
2023-11-20 10:10:50 +0100

    выгрузка еще одного столбца

--- handlers/commands_for_admin.py
+++ handlers/commands_for_admin.py
@@ -33,6 +33,7 @@ async def get_active_users(message: Message, bot: Bot):
                     u.email, 
                     u.city, 
                     u.is_subscription_active,
+                    ump.week_number AS "номер недели питания",
                     ump.start_date AS "дата начала питания",
                     ump.end_date AS "дата окончания питания",
                     MAX(p.timestamp) AS "дата последней оплаты"
@@ -40,14 +41,14 @@ async def get_active_users(message: Message, bot: Bot):
                 LEFT JOIN user_meal_plan ump ON u.telegram_user_id = ump.telegram_user_id
                 LEFT JOIN payments p ON u.telegram_user_id = p.telegram_user_id
                 WHERE u.is_subscription_active = true
-                GROUP BY u.id, ump.start_date, ump.end_date
+                GROUP BY u.id, ump.week_number, ump.start_date, ump.end_date
                 ORDER BY u.id;
             """)
             rows = cursor.fetchall()
 
             df = pd.DataFrame(rows, columns=[
                 "id", "telegram_user_id", "gender", "first_name", "last_name",
-                "birth_date", "phone_number", "email", "city", "is_subscription_active",
+                "birth_date", "phone_number", "email", "city", "is_subscription_active", "номер недели питания",
                 "дата начала питания", "дата окончания питания", "дата последней оплаты"
             ])
 
